e24402f99d52e80a215d6bc063880d91d8c6bdb7
anadunning <anakpedrosa@hotmail.com>
2020-06-14 19:30:52 -0300

    9 Positive Negative or Zero

---
+++ PositiveNegativeZero.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int x;
+
+    cin >> x;
+
+    if (x > 0){
+        cout << "positivo" << endl;
+    }
+    else if (x < 0){
+        cout << "negativo" << endl;
+    }
+    else {
+        cout << "nulo" << endl;
+    }
+
+    return 0;
+}
