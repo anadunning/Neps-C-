d5534a143cc13dc204fb7c3b0717dcd155fb3264
anadunning <anakpedrosa@hotmail.com>
2020-06-22 18:11:41 -0300

    30 Zero or One?

---
+++ ZeroOrOne.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int A, B, C;
+
+    cin >> A >> B >> C;
+
+    if(A == B && A == C){
+        cout << "*" << endl;
+    }
+    else if (A != B && A != C && B == C){
+        cout << "A" << endl;
+    }
+    else if (B != A && B != C && A == C){
+        cout << "B" << endl;
+    }
+    else if (C != A && C != B && A == B){
+        cout << "C" << endl;
+    }
+
+    return 0;
+}
