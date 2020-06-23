84f3a4314ccb8e692bebf9d9c0320cf34b9d2941
anadunning <anakpedrosa@hotmail.com>
2020-06-22 22:01:06 -0300

    34 Medals - ** Several ifs

---
+++ Medals.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int t1, t2, t3;
+
+    cin >> t1 >> t2 >> t3;
+
+    if (t1 < t2 && t2 < t3){
+        cout << "1" << endl;
+        cout << "2" << endl;
+        cout << "3" << endl;
+    }
+    else if (t2 < t1 && t1 < t3){
+        cout << "2" << endl;
+        cout << "1" << endl;
+        cout << "3" << endl;
+    }
+    else if (t3 < t2 && t2 < t1){
+        cout << "3" << endl;
+        cout << "2" << endl;
+        cout << "1" << endl;
+    }
+    else if (t1 < t3 && t3 < t2){
+        cout << "1" << endl;
+        cout << "3" << endl;
+        cout << "2" << endl;
+    }
+    else if (t2 < t3 && t3 < t1){
+        cout << "2" << endl;
+        cout << "3" << endl;
+        cout << "1" << endl;
+    }
+    else if (t3 < t1 && t1 < t2){
+        cout << "3" << endl;
+        cout << "1" << endl;
+        cout << "2" << endl;
+    }
+
+    return 0;
+}
