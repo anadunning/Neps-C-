b1652c723a783a0655a81726c2d967cc89ee949b
anadunning <anakpedrosa@hotmail.com>
2020-06-25 21:35:22 -0300

    Pass Fail or Final exam?

---
+++ PassFailFinal.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    double n1, n2, avg;
+
+    cin >> n1 >> n2;
+
+    avg = ((n1 * 2) + (n2 * 3)) / 5;
+
+    if (avg >= 7.0){
+        cout << "Aprovado" << endl;
+    }
+    else if (avg < 3.0){
+        cout << "Reprovado" << endl;
+    }
+    else {
+        cout << "Final" << endl;
+    }
+
+    return 0;
+}
