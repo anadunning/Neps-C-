42c36470527b8cca37db5cf02a8a533c1187839c
anadunning <anakpedrosa@hotmail.com>
2020-06-30 22:42:12 -0300

    40 Best Student

---
+++ BestStudent.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    double A, B;
+
+    cin >> A >> B;
+
+    if (A <= B){
+        cout << "Pedro" << endl;
+    }
+    else {
+        cout << "Paulo" << endl;
+    }
+
+    return 0;
+}
