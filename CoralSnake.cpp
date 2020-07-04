53dc9200e1150097b3869b6ff7688680e64fcc85
anadunning <anakpedrosa@hotmail.com>
2020-07-03 21:54:00 -0300

    50 How to identify a coral snake with numbers

---
+++ CoralSnake.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int n1, n2, n3, n4;
+
+    cin >> n1 >> n2 >> n3 >> n4;
+
+    if ((n1 == n3) || (n2 == n4)){
+        cout << "V" << endl;
+    }
+    else {
+        cout << "F" << endl;
+    }
+
+    return 0;
+}
